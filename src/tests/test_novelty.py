9897716ebc9974475143d79ee09819706f2de2b1
Berk <69094722+AgricolaC@users.noreply.github.com>
2025-10-08 20:15:12 +0200

    Create test_novelty.py

---
+++ src/tests/test_novelty.py
@@ -0,0 +1,19 @@
+import numpy as np
+from challenge.novelty.pca_index import PCAIndex
+from challenge.novelty.mahalanobis import MahalanobisIndex
+
+def test_pca_index_api():
+    model = 
+    ref =
+    try:
+        model.fit(ref)
+    except NotImplementedError:
+        return  # expected until implemented
+
+def test_mahalanobis_api():
+    model = MahalanobisIndex()
+    ref = 
+    try:
+        model.fit(ref)
+    except NotImplementedError:
+        return
