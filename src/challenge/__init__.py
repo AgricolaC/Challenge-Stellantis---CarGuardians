83bcfd11e69b63bf3a0c3d5857cbede97ce17cf3
Berk <69094722+AgricolaC@users.noreply.github.com>
2025-10-08 20:08:24 +0200

    Create __init__.py

---
+++ src/challenge/__init__.py
@@ -0,0 +1,9 @@
+"""
+Challenge package entry point.
+Exposes subpackages:
+- ingest: data loaders for TDMS/Excel
+- preprocess: optional cleaning/feature steps for in-house processing
+- novelty: novelty indices and scoring
+- visualize: plotting utilities
+- utils: shared paths/params
+"""
