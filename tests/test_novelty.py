a234d09848c423a7ca70ecc5d9024b389a1c9aad
Berk <69094722+AgricolaC@users.noreply.github.com>
2025-10-08 19:17:33 +0200

    Create test_novelty.py

---
+++ tests/test_novelty.py
@@ -0,0 +1,8 @@
+import numpy as np
+from challenge.novelty.pca_index import PCAIndex
+from challenge.novelty.mahalanobis import MahalanobisIndex
+
+def test_pca_index():
+
+def test_mahalanobis_index():
+
