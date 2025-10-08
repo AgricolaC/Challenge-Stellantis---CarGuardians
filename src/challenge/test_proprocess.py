d634e1d4ada6992ccbfcb14ee0d7e7f9ea2dd440
Berk <69094722+AgricolaC@users.noreply.github.com>
2025-10-08 19:36:33 +0200

    Create test_proprocess.py

---
+++ src/challenge/test_proprocess.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from challenge.preprocess import rms, peak_to_peak, basic_clean_df
+
+# def test_rms():
+   
+
+# def test_peak_to_peak():
+   
+
+# def test_basic_clean_df():
+   
