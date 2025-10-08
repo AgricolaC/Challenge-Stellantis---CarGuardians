7a7ececb3a3f8cc979e2314c5e19eea1eab3a72e
Berk <69094722+AgricolaC@users.noreply.github.com>
2025-10-08 20:12:18 +0200

    Delete tests/test_novelty.py

--- tests/test_novelty.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from challenge.novelty.pca_index import PCAIndex
-from challenge.novelty.mahalanobis import MahalanobisIndex
-
-def test_pca_index():
-
-def test_mahalanobis_index():
-
